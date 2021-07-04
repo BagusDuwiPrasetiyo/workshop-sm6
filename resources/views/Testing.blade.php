569211b488edb69d9de2789ed728acd32057b6b3
Bagus Duwi Prasetiyo <39429336+BagusDuwiPrasetiyo@users.noreply.github.com>
2021-07-04 18:37:47 +0700

    commit

--- resources/views/Testing.blade.php
+++ resources/views/Testing.blade.php
@@ -40,75 +40,60 @@
                                         <select name="l_core" id="l_core" class="form-control select2" required style="width: 100%;">
                                             <option value="" selected>--Choose--</option>
                                             <option value="high">HIGH > 37</option>
-                                            <option value="mid">MID <= 37 and >= 36</option>
-                                            <option value="low">LOW < 36</option>
-                                        </select>
+                                            <option value="mid">MID <= 37 and>= 36</option>
+                                            <option value="low">LOW < 36</option> </select> </div> <div class="form-group col-md-3">
+                                                    <label>L SURF</label>
+                                                    <select name="l_surf" id="l_surf" class="form-control select2" required style="width: 100%;">
+                                                        <option value="">--Choose--</option>
+                                                        <option value="high">HIGH > 36.5 </option>
+                                                        <option value="mid">MID <= 36.5 and>= 35 </option>
+                                                        <option value="low">LOW < 35</option> </select> </div> <div class="form-group col-md-3">
+                                                                <label>L 02</label>
+                                                                <select name="l_o2" id="l_o2" class="form-control select2" required style="width: 100%;">
+                                                                    <option value="">--Choose--</option>
+                                                                    <option value="excellent">EXCELLENT >= 98%</option>
+                                                                    <option value="good">GOOD >= 90% and < 98%</option> <option value="fair">FAIR >= 80% and < 90%</option> <option value="poor">POOR < 80%</option> </select> </div> <div class="form-group col-md-3">
+                                                                                    <label>L BP</label>
+                                                                                    <select name="l_bp" id="l_bp" class="form-control select2" required style="width: 100%;">
+                                                                                        <option value="">--Choose--</option>
+                                                                                        <option value="high">HIGH > 130/90</option>
+                                                                                        <option value="mid">MID <= 130/90 and>= 90/70</option>
+                                                                                        <option value="low">LOW < 90/70 </option> </select> </div> </div> <div class="row">
+                                                                                                <div class="form-group col-md-3">
+                                                                                                    <label>SURF STBL</label>
+                                                                                                    <select name="surf_stbl" id="surf_stbl" class="form-control select2" required style="width: 100%;">
+                                                                                                        <option value="">--Choose--</option>
+                                                                                                        <option value="stable">STABLE</option>
+                                                                                                        <option value="mod-stable">MOD-STABLE</option>
+                                                                                                        <option value="unstable">UNSTABLE</option>
+                                                                                                    </select>
+                                                                                                </div>
+                                                                                                <div class="form-group col-md-3">
+                                                                                                    <label>CORE STBL</label>
+                                                                                                    <select name="core_stbl" id="core_stbl" class="form-control select2" required style="width: 100%;">
+                                                                                                        <option value="">--Choose--</option>
+                                                                                                        <option value="stable">STABLE</option>
+                                                                                                        <option value="mod-stable">MOD-STABLE</option>
+                                                                                                        <option value="unstable">UNSTABLE</option>
+                                                                                                    </select>
+                                                                                                </div>
+                                                                                                <div class="form-group col-md-3">
+                                                                                                    <label>BP STBL</label>
+                                                                                                    <select name="bp_stbl" id="bp_stbl" class="form-control select2" required style="width: 100%;">
+                                                                                                        <option value="">--Choose--</option>
+                                                                                                        <option value="stable">STABLE</option>
+                                                                                                        <option value="mod-stable">MOD-STABLE</option>
+                                                                                                        <option value="unstable">UNSTABLE</option>
+                                                                                                    </select>
+                                                                                                </div>
+                                                                                                <div class="form-group col-md-3">
+                                                                                                    <label>COMFORT</label>
+                                                                                                    <input type="number" name="comfort" id="comfort" class="form-control" required>
+                                                                                                </div>
+                                                                                                <input type="hidden" id="resultA" name="resultA">
+                                                                                                <input type="hidden" id="resultS" name="resultS">
+                                                                                                <input type="hidden" id="result" name="result">
                                     </div>
-                                    <div class="form-group col-md-3">
-                                        <label>L SURF</label>
-                                        <select name="l_surf" id="l_surf" class="form-control select2" required style="width: 100%;">
-                                            <option value="">--Choose--</option>
-                                            <option value="high">HIGH > 36.5 </option>
-                                            <option value="mid">MID <= 36.5 and >= 35 </option>
-                                            <option value="low">LOW < 35</option>
-                                        </select>
-                                    </div>
-                                    <div class="form-group col-md-3">
-                                        <label>L 02</label>
-                                        <select name="l_o2" id="l_o2" class="form-control select2" required style="width: 100%;">
-                                            <option value="">--Choose--</option>
-                                            <option value="excellent">EXCELLENT >= 98%</option>
-                                            <option value="good">GOOD >= 90% and < 98%</option>
-                                            <option value="fair">FAIR >= 80% and < 90%</option>   
-                                            <option value="poor">POOR < 80%</option>
-                                        </select>
-                                    </div>
-                                    <div class="form-group col-md-3">
-                                        <label>L BP</label>
-                                        <select name="l_bp" id="l_bp" class="form-control select2" required style="width: 100%;">
-                                            <option value="">--Choose--</option>
-                                            <option value="high">HIGH > 130/90</option>
-                                            <option value="mid">MID <= 130/90 and >= 90/70</option>
-                                            <option value="low">LOW < 90/70</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="form-group col-md-3">
-                                        <label>SURF STBL</label>
-                                        <select name="surf_stbl" id="surf_stbl" class="form-control select2" required style="width: 100%;">
-                                            <option value="">--Choose--</option>
-                                            <option value="stable">STABLE</option>
-                                            <option value="mod-stable">MOD-STABLE</option>
-                                            <option value="unstable">UNSTABLE</option>
-                                        </select>
-                                    </div>
-                                    <div class="form-group col-md-3">
-                                        <label>CORE STBL</label>
-                                        <select name="core_stbl" id="core_stbl" class="form-control select2" required style="width: 100%;">
-                                            <option value="">--Choose--</option>
-                                            <option value="stable">STABLE</option>
-                                            <option value="mod-stable">MOD-STABLE</option>
-                                            <option value="unstable">UNSTABLE</option>
-                                        </select>
-                                    </div>
-                                    <div class="form-group col-md-3">
-                                        <label>BP STBL</label>
-                                        <select name="bp_stbl" id="bp_stbl" class="form-control select2" required style="width: 100%;">
-                                            <option value="">--Choose--</option>
-                                            <option value="stable">STABLE</option>
-                                            <option value="mod-stable">MOD-STABLE</option>
-                                            <option value="unstable">UNSTABLE</option>
-                                        </select>
-                                    </div>
-                                    <div class="form-group col-md-3">
-                                        <label>COMFORT</label>
-                                        <input type="number" name="comfort" id="comfort" class="form-control" required>
-                                    </div>
-                                    <input type="hidden" id="resultA" name="resultA">
-                                    <input type="hidden" id="resultS" name="resultS">
-                                    <input type="hidden" id="result" name="result">
-                                </div>
                             </form>
                         </div>
                     </div>
